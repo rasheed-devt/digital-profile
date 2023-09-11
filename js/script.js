f2eb8713a7d7bd5660206d58e0a10a407c3c2704
rasheed-devt <marasheeddev@gmail.com>
2023-09-11 15:44:40 +0530

    1

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*========= typed js ========*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Educator', 'Blogger'],
+    strings: ['Data Analyst Expert', 'Machine Learning Engineer', 'Educator'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
